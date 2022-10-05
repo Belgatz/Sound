b351eaecc8919d644a16b4e47618c2922238ac21
Belgatz <giraffekaku7@gmail.com>
2022-10-04 23:27:58 -0400

    Animation Commit

--- CatWalk/Assets/Scripts/NewBehaviourScript.cs
+++ CatWalk/Assets/Scripts/NewBehaviourScript.cs
@@ -4,43 +4,44 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    public AudioClip musicClipOne;
+  public AudioClip musicClipOne;
+  public AudioClip musicClipTwo;
+  public AudioSource musicSource;
+  Animator anim;
 
-    public AudioClip musicClipTwo;
-
-    public AudioSource musicSource;
-    // Start is called before the first frame update
     void Start()
     {
-        
+       anim = GetComponent<Animator>();   
     }
 
  // Update is called once per frame
 void Update()
 {
+
      if (Input.GetKeyDown(KeyCode.W))
         {
           musicSource.clip = musicClipOne;
           musicSource.Play();
-
+          anim.SetInteger("State", 1);
          }
 
      if (Input.GetKeyUp(KeyCode.W))
         {
           musicSource.Stop();
-
+          anim.SetInteger("State", 0);
          }
 
      if (Input.GetKeyDown(KeyCode.R))
         {
           musicSource.clip = musicClipTwo;
           musicSource.Play();
+          anim.SetInteger("State", 2);
          }
 
      if (Input.GetKeyUp(KeyCode.R))
         {
           musicSource.Stop();
-
+          anim.SetInteger("State", 0);
          }
 
      if (Input.GetKeyDown(KeyCode.L))
